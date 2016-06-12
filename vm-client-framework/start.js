182be94f3e984a37995ae0bdc33894224c851c58
caohonghiep <github761610>
2016-06-12 07:57:32 +0700

    init

--- vm-client-framework/start.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-app.use(express.static('public_html'));
-app.listen(3000, function () {
-  console.log('Main App running on port: 3000' );
-});
